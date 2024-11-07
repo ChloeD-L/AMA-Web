2dc167197ea2ae2faae476155f3ec992c5321fd4
ChloeD-L <linduan1018@gmail.com>
2024-11-07 20:09:01 +1100

    feat: export config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/public_html", // Only if you're hosting in a subdirectory like "/public_html"
+  images: {
+    unoptimized: true, // Important if images are stored locally and not on external CDN
+  },
+  reactStrictMode: true,
+  trailingSlash: true, // Ensures a trailing slash for each route (useful for static exports)
+};
 
 export default nextConfig;
